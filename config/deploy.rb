377a4d17a3e4ad55914bf358fc7395ade03c38c7
yuki-noguchi <y.n.10191@gmail.com>
2017-05-24 17:21:41 +0900

    Set deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,22 @@
-# config valid only for current version of Capistrano
-lock "3.8.1"
+lock '3.8.1'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'note'
+set :repo_url, 'git@github.com:yuki-noguchi/note.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/keypair.pem']
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
