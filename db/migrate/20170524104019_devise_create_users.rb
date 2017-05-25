4261f8cd28f4ab410042a7192f53b65e5fa58768
yuki-noguchi <y.n.10191@gmail.com>
2017-05-25 18:35:00 +0900

    Set devise

--- db/migrate/20170524104019_devise_create_users.rb
+++ db/migrate/20170524104019_devise_create_users.rb
@@ -30,12 +30,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :nickname
+      t.string :note_id, unique: true
+      t.string :avatar
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :nickname
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
