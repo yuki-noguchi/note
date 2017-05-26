96bfba1edbcfc487a98ebf241b4f1fc7ca100ac3
yuki-noguchi <y.n.10191@gmail.com>
2017-05-26 17:19:21 +0900

    Set helpers
    To display user nickname, avatar, and article image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def user_nickname(instanse)
+    if instanse == @user
+      user = @user
+    else
+      user = instanse.user
+    end
+    if user.nickname?
+      return user.nickname
+    else
+      return user.email.sub(/@.+/, "")
+    end
+  end
+
+  def article_image(article)
+    content_tag :a, href: article_path(article) do
+      concat (content_tag :img, "", src: article.image) if article.image
+    end
+  end
+
+  def user_avatar(instanse)
+    if instanse == current_user
+      user = current_user
+    elsif instanse == @user
+      user = @user
+    else
+      user = instanse.user
+    end
+    if user.avatar?
+      content_tag :img, "", src: user.avatar
+    else
+      content_tag :img, "", src: '/assets/prof.jpg'
+    end
+  end
 end
