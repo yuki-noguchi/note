db5f0329d4c33d7a5a0d500e54638897ecb2f928
yuki-noguchi <y.n.10191@gmail.com>
2017-05-26 20:23:13 +0900

    Create helper of edit

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,4 +4,12 @@ module ArticlesHelper
       link_to '削除する', article, method: :delete, data: { confirm: '本当に削除してもよろしいですか？' }
     end
   end
+
+  def edit(user)
+    if user.id == current_user.id
+      content_tag :div, class: 'note__user__edit' do
+        concat (content_tag :a, '編集', href: edit_article_path)
+      end
+    end
+  end
 end
