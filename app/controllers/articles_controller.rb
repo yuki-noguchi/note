fbf51e0402b8b3ba20185d15524276e70d5930b0
yuki-noguchi <y.n.10191@gmail.com>
2017-05-27 20:20:20 +0900

    Do image column permitted

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :body).merge(user_id: current_user.id)
+    params.require(:article).permit(:title, :body, :image).merge(user_id: current_user.id)
   end
 
   def set_article
