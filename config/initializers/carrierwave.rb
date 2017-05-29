2cbd36524e5ad4b3e520c05adc72864c6a8c84b7
yuki-noguchi <y.n.10191@gmail.com>
2017-05-29 21:03:57 +0900

    edit deploy.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    aws_access_key_id: aws['ACCESS_KEY_ID'],
+    aws_secret_access_key: aws['SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
   config.fog_public = true
-  config.fog_directory  = ENV['BUCKET']
-  config.asset_host = ENV['AWS_S3_URL']
+  config.fog_directory  = aws['BUCKET']
+  config.asset_host = aws['AWS_S3_URL']
 
 end
