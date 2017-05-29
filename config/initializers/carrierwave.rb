0af08fec3a88e0bb241aab45d4338cd0e9ff97ff
yuki-noguchi <y.n.10191@gmail.com>
2017-05-29 21:42:45 +0900

    User secrets.yml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: aws['ACCESS_KEY_ID'],
-    aws_secret_access_key: aws['SECRET_ACCESS_KEY'],
+    aws_access_key_id: Rails.application.secrets.access_key_id,
+    aws_secret_access_key: Rails.application.secrets.secret_access_key,
     region: 'ap-northeast-1'
   }
   config.fog_public = true
-  config.fog_directory  = aws['BUCKET']
-  config.asset_host = aws['AWS_S3_URL']
+  config.fog_directory  = Rails.application.secrets.bucket
+  config.asset_host = Rails.application.secrets.s3_url
 
 end
