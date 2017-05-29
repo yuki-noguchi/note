9142abec54679a5c904d995f3e9f8320950b192a
yuki-noguchi <y.n.10191@gmail.com>
2017-05-29 21:12:05 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'note'
 set :repo_url, 'git@github.com:yuki-noguchi/note.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, %{ secrets.yml }
+set :linked_files, %w{ secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
