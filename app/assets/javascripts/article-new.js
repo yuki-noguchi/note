91ac13b30b04ed401d363ba9c5a616df3ba681f7
yuki-noguchi <y.n.10191@gmail.com>
2017-06-01 19:18:14 +0900

    Create article-new.js
    To display modal for buying note

---
+++ app/assets/javascripts/article-new.js
@@ -0,0 +1,42 @@
+$(function(){
+  function centeringModalSyncer(){
+    var w = $(window).width();
+    var h = $(window).height();
+    var cw = $(".modal-window").outerWidth(true);
+    var ch = $(".modal-window").outerHeight(true);
+    var pxleft = ((w - cw)/2);
+    var pxtop = ((h - ch)/2);
+    $(".modal-window").fadeIn('slow');
+    $(".modal-window").css({"left": pxleft + "px"});
+    $(".modal-window").css({"top": pxtop + "px"});
+  }
+
+  function bodyLimit(){
+    $('.modal-window__body-limit').show();
+    var bodyLength = $('.create-note__main__body').val().length;
+    $('.body-length').append(bodyLength + '文字');
+  }
+
+  $('.create-note__header__post').click(function(){
+    $(this).blur();
+    if($('.modal-overlay')[0]) return false;
+    $('body').append('<div class="modal-overlay"></div>');
+    $('.modal-overlay').fadeIn('slow');
+    centeringModalSyncer();
+  });
+
+  $("body").on("click", ".modal-overlay, .modal-close", function(){
+    $(".modal-overlay").remove();
+    $(".modal-window").hide();
+  });
+  $('.free_or_paid').click(function(){
+    var check = $('input[name="price"]:checked').val();
+    if(check === 'free'){
+      $('.modal-window__price__field').hide();
+      $('.modal-window__body-limit').hide();
+    } else {
+      $('.modal-window__price__field').show();
+      bodyLimit();
+    }
+  });
+});
