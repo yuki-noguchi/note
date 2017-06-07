ee1cc3439a6313daf89a5fa42f9353427a4a25a9
yuki-noguchi <y.n.10191@gmail.com>
2017-06-07 18:50:08 +0900

    Add associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
   has_many :articles, foreign_key: :author_id, dependent: :delete_all
   has_many :purchases, foreign_key: :buyer_id
   has_many :paid_articles, through: :purchases, source: :article
+
+  has_many :messages, foreign_key: :talker_id, dependent: :delete_all
+  has_many :group_talkers, foreign_key: :talker_id
+  has_many :groups, through: :group_talkers
+  accepts_nested_attributes_for :group_talkers
   mount_uploader :avatar, ImageUploader
   mount_uploader :head_image, ImageUploader
 end
